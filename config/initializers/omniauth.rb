eb15a86fe13386f7f9c9ad4b6aa40d335c2fb2ca
amritlakra <amritlakra@live.com>
2013-10-03 01:36:09 +0530

    stuff modified1

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, ENV['id'], ENV['key']
+    provider :facebook,"1393897487508946","dbeb350dc356f82d8059343af3268cae"
 end
