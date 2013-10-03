a5694099112fa0db3356c5aa68ba1b3c9e27e0a6
amritlakra <amritlakra@live.com>
2013-10-03 12:31:59 +0530

    needed

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-    provider :facebook, '1418043385077601', '38889494571b11bc18ebbae192e86673'
+    provider :facebook, ENV['FACEBOOK_ID'], ENV['FACEBOOK_SECRET']
 end
