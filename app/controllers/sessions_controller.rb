7e37706570e3f8ad4bb7b4ef3cf8f435ede01400
amritlakra <amritlakra@live.com>
2013-10-05 21:31:58 +0530

    debugged

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,17 +6,13 @@ end
 def create
     user = User.from_omniauth(env["omniauth.auth"])
     session[:user_id] = user.id
-	session[:token]=getToken(env["omniauth.auth"])
+	session[:token]=env["omniauth.auth"]["credentials"]["token "]
     redirect_to root_url, notice: "Signed in! by"+user.provider
 end
 
 def destroy
     session[:user_id] = nil
     redirect_to root_url, notice: "Signed out!"
-end
-
-private def getToken(auth)
-auth["credentials"]["token "]
 end
 
 end
