9cac83cce3b137fee3177249c8a9ab804faa229a
amritlakra <amritlakra@live.com>
2013-10-05 22:25:38 +0530

    stilllll

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,7 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
-	@fbtoken = session[:token] 
+	
   end
   helper_method :current_user
 end
