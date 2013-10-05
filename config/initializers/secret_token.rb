7714c0e25b13de9754ab5d1aa8f4170292a26043
amritlakra <amritlakra@live.com>
2013-10-05 16:44:19 +0530

    serure the app

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Oauthprototype10::Application.config.secret_key_base = '67e8ce45598498a925bade1ce52d3625b3394d0c9ed6eda0c49712c0339d9dc69c250f1f8886d825b4cf11a8b7e5d5046a05b0282993a4cc49f5757850a3595e'
+Oauthprototype10::Application.config.secret_key_base = ENV['SECRET_TOKEN']
