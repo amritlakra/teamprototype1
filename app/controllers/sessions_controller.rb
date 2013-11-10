26030d5b517f42701008412bd0c45f683e2b67b4
amritlakra <amritlakra@live.com>
2013-11-10 14:54:32 +0530

    test2

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ end
 
 def create
     user = User.from_omniauth(env["omniauth.auth"])
+    @tokhash=env["omniauth.auth"])
     session[:user_id] = user.id
 	
     redirect_to root_url, notice: "Signed in! by"+user.provider
@@ -12,6 +13,7 @@ end
 
 def destroy
     session[:user_id] = nil
+    @tokhash=nil
     redirect_to root_url, notice: "Signed out!"
 end
 
