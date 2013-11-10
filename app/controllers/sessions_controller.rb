5984516f5c0bed0f0817ea2f5467cebcec3f6d24
amritlakra <amritlakra@live.com>
2013-11-10 21:31:40 +0530

    updtcmnt

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,6 @@ end
 
 def create
     user = User.from_omniauth(env["omniauth.auth"])
-    @tokhash=request.env["omniauth.auth"]
     session[:user_id] = user.id
 	redirect_to root_url, notice: "Signed in! by"+user.provider
 end
